d7fa3d8932b46994b89862b0c6fd867de533850f
Fabiano Lecce <fabiano.lecce@outlook.com>
2020-12-07 14:14:51 +0100

    WhereClause unit tests added

---
+++ src/OpenDataWrapper.Tests/WhereClause_ToString.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenDataWrapper.Tests
+{
+    [TestClass]
+    public class WhereClause_ToString
+    {
+        [TestMethod]
+        public void Simple_Logical_And_Expression()
+        {
+            var where = new WhereClause(
+                new AndElse(
+                    new FilterTriple("scode", "eq", "BZ5"), 
+                    new FilterTriple("scode", "eq", "BZ6")
+                )
+            );
+
+            var expected = "and(scode.eq.BZ5,scode.eq.BZ6)";
+            var result = where.ToString();
+
+            Assert.IsTrue(!string.IsNullOrEmpty(result));
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Nested_Logical_Expression()
+        {
+            var where = new WhereClause(
+                new AndElse(
+                    new FilterTriple("scode", "eq", "BZ5"),
+                    new OrElse(new FilterTriple("tname", "eq", "name1"), new FilterTriple("tname", "eq", "name2")))
+                );
+
+            var expected = "and(scode.eq.BZ5,or(tname.eq.name1,tname.eq.name2))";
+            var result = where.ToString();
+
+            Assert.IsTrue(!string.IsNullOrEmpty(result));
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
