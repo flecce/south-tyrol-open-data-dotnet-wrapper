0d1f8e43aed5620e174450356a103d98067fa49a
Fabiano Lecce <fabiano.lecce@outlook.com>
2020-12-07 14:16:13 +0100

    Datetime json deserialize converter added

---
+++ src/OpenDataWrapper/JsonConverters/DateTimeOffsetConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenDataWrapper.JsonConverters
+{
+    public class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+    {
+        public override DateTimeOffset Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            var cstIndex = reader.GetString().IndexOf("+");
+            return DateTimeOffset.ParseExact(
+                reader.GetString().Substring(0, (cstIndex > 0 ? cstIndex : 0)),
+                "yyyy-MM-dd HH:mm:ss.fff",
+                CultureInfo.InvariantCulture);
+        }
+                
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTimeOffset dateTimeValue,
+            JsonSerializerOptions options) =>
+                writer.WriteStringValue(dateTimeValue.ToString(
+                    "yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture));
+    }
+}
