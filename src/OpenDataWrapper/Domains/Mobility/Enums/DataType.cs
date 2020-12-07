1aae2b0e921a70030bc18ea1209bfbdcfc34bfc3
Fabiano Lecce <fabiano.lecce@outlook.com>
2020-12-07 14:15:48 +0100

    Generic enums added

---
+++ src/OpenDataWrapper/Domains/Mobility/Enums/DataType.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenDataWrapper.Domains.Mobility.Enums
+{
+    public static class DataType
+    {
+        public static class EnvironmentStation
+        {
+            public static string All = "*";
+            public static string NitrogenDioxide = "nitrogen-dioxide";
+        }
+    }
+}
