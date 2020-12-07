fe3cc60677948ed579fe54663a040c3d223135fc
Fabiano Lecce <fabiano.lecce@outlook.com>
2020-12-07 14:15:12 +0100

    Environment station data unit tests added

--- src/OpenDataWrapper.Tests/MobilityService_Get.cs
+++ src/OpenDataWrapper.Tests/MobilityService_Get.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenDataWrapper.Domains.Mobility;
 using OpenDataWrapper.Domains.Mobility.Dto;
+using OpenDataWrapper.Domains.Mobility.Dto.Metadata;
 using OpenDataWrapper.Domains.Mobility.Enums;
 using OpenDataWrapper.Enums;
 
@@ -13,16 +14,19 @@ namespace OpenDataWrapper.Tests
         public void Get_Bicycle_Station_Data()
         {
             var mobilityService = new MobilityService();
-            var result = mobilityService.Get<MobilityResponse<BicycleStation, Bicycle>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.Bicycle })).Result;
+            var result = mobilityService.Get<MobilityStationTypeResponse<BicycleStationMetadata, BicycleMetadata>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.Bicycle })).Result;
 
-            Assert.IsTrue(result.Data.Count > 0);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Offset, 0);
+            Assert.AreEqual(result.Limit, 200);
+            Assert.IsNotNull(result.Data);
         }
 
         [TestMethod]
         public void Get_ECharging_Station_Data()
         {
             var mobilityService = new MobilityService();
-            var result = mobilityService.Get<MobilityResponse<ChargingStation>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.EChargingStation })).Result;
+            var result = mobilityService.Get<MobilityStationTypeResponse<ChargingStation>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.EChargingStation })).Result;
 
             Assert.IsTrue(result.Data.Count > 0);
         }
@@ -31,9 +35,34 @@ namespace OpenDataWrapper.Tests
         public void Get_ECharging_Station_And_ECharging_Plug_Data()
         {
             var mobilityService = new MobilityService();
-            var result = mobilityService.Get<MobilityResponse<ChargingStation>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.EChargingStation, StationType.ECharingPlug })).Result;
-            
-            Assert.IsTrue(result.Data.Count > 0);
+            var result = mobilityService.Get<MobilityStationTypeResponse<ChargingStation>>(RequestBase.Create(RepresentationType.Flat, new string[] { StationType.EChargingStation, StationType.ECharingPlug })).Result;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Offset, 0);
+            Assert.AreEqual(result.Limit, 200);
+            Assert.IsNotNull(result.Data);
+        }
+
+        [TestMethod]
+        public void Get_Environment_Station_Measurement_Data()
+        {
+            var mobilityService = new MobilityService();
+            var result = mobilityService.Get<MobilityDataTypeResponse<NullMetadata>>(
+                RequestBase.Create(
+                    RepresentationType.Flat,
+                    new string[] { StationType.EnvironmentStation },
+                    new string[] { DataType.EnvironmentStation.All },
+                    null,
+                    new WhereClause(new OrElse(
+                        new FilterTriple("scode", OperatorType.Eq, "BZ5"),
+                        new FilterTriple("scode", OperatorType.Eq, "BZ6"))
+                    )
+                )).Result;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Offset, 0);
+            Assert.AreEqual(result.Limit, 200);
+            Assert.IsNotNull(result.Data);
         }
     }
 }
